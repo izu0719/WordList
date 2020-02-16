07e4b67fd6c69185c9f4cdb7d2b6719cc68ce908
Izumi Sakamoto <izu.kitty.07190719@gmail.com>
2020-02-16 22:00:39 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 坂本 泉 on 2020/02/16.
+//  Copyright © 2020 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
