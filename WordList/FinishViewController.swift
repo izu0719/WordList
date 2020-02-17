3bfd6ea814e4fdb81248827e32336462195b0d2c
Izumi Sakamoto <izu.kitty.07190719@gmail.com>
2020-02-17 11:01:42 +0900

    更新します

---
+++ WordList/FinishViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FinishViewController.swift
+//  WordList
+//
+//  Created by 坂本 泉 on 2020/02/16.
+//  Copyright © 2020 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class FinishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
